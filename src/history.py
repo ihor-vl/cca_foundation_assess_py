840e6b4c125a0877f0f65800ccd0d768b0d3151c
Scandie <grizly.vl@gmail.com>
2024-03-18 18:37:05 +0200

    Add `List orders shipped to an address` feature
    * Add tests for `List orders shipped to an address` feature

--- src/history.py
+++ src/history.py
@@ -14,3 +14,13 @@ class SalesHistory:
             order for order in self.orders
             if any(item.product.id == product_id for item in order.items)
         ]
+
+    def list_orders_by_address(self, house, street, city, postcode, country) -> list[Order]:
+        return [
+            order for order in self.orders
+            if order.shipping_address.house == house
+            and order.shipping_address.street == street
+            and order.shipping_address.city == city
+            and order.shipping_address.postcode == postcode
+            and order.shipping_address.country == country
+        ]
--- test/test_history.py
+++ test/test_history.py
@@ -142,3 +142,54 @@ def test_add_order(history, orders, history_orders_after_confirmation):
 )
 def test_list_orders_by_product(history, product_id, expected_orders):
     assert history.list_orders_by_product(product_id) == expected_orders
+
+
+@pytest.mark.parametrize(
+    "history, house, street, city, postcode, country, expected_orders",
+    [
+        pytest.param(
+            SalesHistory(orders=[
+                Order(
+                    shipping_address=Address(
+                        "dummy-house", "dummy-street",
+                        "dummy-city", "dummy-zip", Country.UNITED_KINGDOM.value
+                    ),
+                    items=[
+                        Item(Product(1, "dummy-description-1", 12.0), 35),
+                        Item(Product(2, "another-dummy-description-1", 15.0), 4),
+                    ],
+                ),
+                Order(
+                    shipping_address=Address(
+                        "dummy-house", "dummy-street",
+                        "dummy-city", "dummy-zip", Country.UKRAINE.value
+                    ),
+                    items=[
+                        Item(Product(3, "dummy-description-2", 10.0), 10),
+                        Item(Product(4, "another-dummy-description-3", 12.0), 7),
+                    ],
+                ),
+            ]),
+            "dummy-house",
+            "dummy-street",
+            "dummy-city",
+            "dummy-zip",
+            Country.UKRAINE.value,
+            [
+                Order(
+                    shipping_address=Address(
+                        "dummy-house", "dummy-street",
+                        "dummy-city", "dummy-zip", Country.UKRAINE.value
+                    ),
+                    items=[
+                        Item(Product(3, "dummy-description-2", 10.0), 10),
+                        Item(Product(4, "another-dummy-description-3", 12.0), 7),
+                    ],
+                ),
+            ],
+            id="List orders by address"
+        ),
+    ]
+)
+def test_list_orders_by_address(history, house, street, city, postcode, country, expected_orders):
+    assert history.list_orders_by_address(house, street, city, postcode, country) == expected_orders
