7ec251d5c0d3b47d0ed479436bf2e262f332b5e2
Scandie <grizly.vl@gmail.com>
2024-03-18 18:50:19 +0200

    Refactor calculate_shipping

--- src/shipping.py
+++ src/shipping.py
@@ -1,6 +1,25 @@
 import requests as requests
 
 
+SHIPPING_RATES_BY_REGION = {
+    "UK": {
+        "threshold": 120.0,
+        "under_threshold": 4.99,
+        "over_threshold": 0.0
+    },
+    "EU": {
+        "threshold": 100.0,
+        "under_threshold": 8.99,
+        "over_threshold": 4.99
+    },
+    "OTHER": {
+        "threshold": 200.0,
+        "under_threshold": 9.99,
+        "over_threshold": 5.99
+    }
+}
+
+
 def get_region_by_country(country):
     url = "https://npovmrfcyzu2gu42pmqa7zce6a0zikbf.lambda-url.eu-west-2.on.aws/?country=" + country
 
@@ -11,22 +30,9 @@ def get_region_by_country(country):
 
 
 def calculate_shipping(region, order_total):
-    shipping = 0.0
-
-    if region == "UK":
-        if order_total < 120.0:
-            shipping = 4.99
-
-    if region == "EU":
-        if order_total < 100:
-            shipping = 8.99
-        else:
-            shipping = 4.99
-
-    if region == "OTHER":
-        if order_total < 200:
-            shipping = 9.99
-        else:
-            shipping = 5.99
-
-    return shipping
+    shipping_rates = SHIPPING_RATES_BY_REGION[region]
+    return (
+        shipping_rates["under_threshold"]
+        if order_total < shipping_rates["threshold"]
+        else shipping_rates["over_threshold"]
+    )
