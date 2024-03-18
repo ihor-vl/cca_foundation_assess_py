16de8530691202be40908b20d8860334df906af0
Scandie <grizly.vl@gmail.com>
2024-03-18 17:25:58 +0200

    Add fast unit tests for shipping cost calculation

--- test/test_shipping_costs.py
+++ test/test_shipping_costs.py
@@ -1,5 +1,9 @@
 import subprocess
 
+import pytest
+
+from src.shipping import calculate_shipping
+
 
 def compare_golden_master(stdout_lines, expected_results):
     assert len(stdout_lines) == len(expected_results)
@@ -15,3 +19,19 @@ def test_shipping_costs_match_expected_results():
     stdout_lines = process.stdout.splitlines()
 
     compare_golden_master(stdout_lines, expected_shipping_costs)
+
+
+@pytest.mark.parametrize(
+    'region, order_total, expected_shipping_cost',
+    [
+        ('UK', 99.99, 4.99),
+        ('UK', 100.00, 0.0),
+        ('EU', 99.99, 8.99),
+        ('EU', 100.00, 4.99),
+        ('OTHER', 99.99, 9.99),
+        ('OTHER', 100.00, 9.99),
+    ]
+)
+def test_shipping_costs(region, order_total, expected_shipping_cost):
+    shipping_cost = calculate_shipping(region, order_total)
+    assert shipping_cost == expected_shipping_cost
