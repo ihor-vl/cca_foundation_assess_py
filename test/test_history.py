6a64156b57a98d324ec8bded4b06a1f0594b9c80
Scandie <grizly.vl@gmail.com>
2024-03-18 18:21:50 +0200

    Include tests

--- test/test_history.py
+++ test/test_history.py
@@ -1,28 +1,106 @@
 import pytest
 
+from src.address import Address
+from src.countries import Country
 from src.history import SalesHistory
+from src.order import Order, Item
+from src.product import Product
+from test.utils import compare_items
 
 
 def compare_history(orders, history_orders_after_confirmation):
     assert len(orders) == len(history_orders_after_confirmation)
-    for order in orders:
-        expected_order = next(
-            (expected_order for expected_order in history_orders_after_confirmation
-             if expected_order.id == order.id),
-            False
-        )
-        assert expected_order
+    for i, order in enumerate(orders):
+        expected_order = history_orders_after_confirmation[i]
+        assert expected_order.shipping_address == order.shipping_address
         assert order.shipping_address == expected_order.shipping_address
-        assert order.items == expected_order.items
+        compare_items(order.items, expected_order.items)
 
 
 @pytest.mark.parametrize(
-    "orders, history_orders_after_confirmation",
-    []
+    "history, orders, history_orders_after_confirmation",
+    [
+        pytest.param(
+            SalesHistory(orders=[]),
+            [
+                Order(
+                    shipping_address=Address(
+                        "dummy-house", "dummy-street",
+                        "dummy-city", "dummy-zip", Country.UKRAINE.value
+                    ),
+                    items=[
+                        Item(Product(1, "dummy-description", 10.0), 10),
+                        Item(Product(2, "another-dummy-description", 12.0), 7),
+                    ],
+                ),
+            ],
+            [
+                Order(
+                    shipping_address=Address(
+                        "dummy-house", "dummy-street",
+                        "dummy-city", "dummy-zip", Country.UKRAINE.value
+                    ),
+                    items=[
+                        Item(Product(1, "dummy-description", 10.0), 10),
+                        Item(Product(2, "another-dummy-description", 12.0), 7),
+                    ],
+                ),
+            ],
+            id="Add one new order"
+        ),
+        pytest.param(
+            SalesHistory(orders=[
+                Order(
+                    shipping_address=Address(
+                        "dummy-house", "dummy-street",
+                        "dummy-city", "dummy-zip", Country.UNITED_KINGDOM.value
+                    ),
+                    items=[
+                        Item(Product(1, "dummy-description-1", 12.0), 35),
+                        Item(Product(2, "another-dummy-description-1", 15.0), 4),
+                    ],
+                ),
+            ]),
+            [
+                Order(
+                    shipping_address=Address(
+                        "dummy-house", "dummy-street",
+                        "dummy-city", "dummy-zip", Country.UKRAINE.value
+                    ),
+                    items=[
+                        Item(Product(1, "dummy-description", 10.0), 10),
+                        Item(Product(2, "another-dummy-description", 12.0), 7),
+                    ],
+                ),
+            ],
+            [
+                Order(
+                    shipping_address=Address(
+                        "dummy-house", "dummy-street",
+                        "dummy-city", "dummy-zip", Country.UNITED_KINGDOM.value
+                    ),
+                    items=[
+                        Item(Product(1, "dummy-description-1", 12.0), 35),
+                        Item(Product(2, "another-dummy-description-1", 15.0), 4),
+                    ],
+                ),
+                Order(
+                    shipping_address=Address(
+                        "dummy-house", "dummy-street",
+                        "dummy-city", "dummy-zip", Country.UKRAINE.value
+                    ),
+                    items=[
+                        Item(Product(1, "dummy-description", 10.0), 10),
+                        Item(Product(2, "another-dummy-description", 12.0), 7),
+                    ],
+                ),
+            ],
+            id="Add one new order to the existing history"
+        ),
+    ]
 )
-@pytest.mark.skip(reason="Not ready yet")
-def test_add_order(orders, history_orders_after_confirmation):
-    history = SalesHistory(orders=[])
-    history.add_order(orders)
+def test_add_order(history, orders, history_orders_after_confirmation):
+    for order in orders:
+        history.add_order(order)
 
     compare_history(history.orders, history_orders_after_confirmation)
