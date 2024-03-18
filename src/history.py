f2b0e7f81b4709616fc7ded7e6913f1e0c3b43d6
Scandie <grizly.vl@gmail.com>
2024-03-18 18:32:25 +0200

    Add List orders of specific products feature
    * Add tests for list products of specific feature

--- src/history.py
+++ src/history.py
@@ -8,3 +8,9 @@ class SalesHistory:
 
     def add_order(self, order: Order) -> None:
         self.orders.append(order)
+
+    def list_orders_by_product(self, product_id: int) -> list[Order]:
+        return [
+            order for order in self.orders
+            if any(item.product.id == product_id for item in order.items)
+        ]
--- test/test_history.py
+++ test/test_history.py
@@ -95,3 +95,50 @@ def test_add_order(history, orders, history_orders_after_confirmation):
         history.add_order(order)
 
     compare_history(history.orders, history_orders_after_confirmation)
+
+
+@pytest.mark.parametrize(
+    "history, product_id, expected_orders",
+    [
+        pytest.param(
+            SalesHistory(orders=[
+                Order(
+                    shipping_address=Address(
+                        "dummy-house", "dummy-street",
+                        "dummy-city", "dummy-zip", Country.UNITED_KINGDOM.value
+                    ),
+                    items=[
+                        Item(Product(1, "dummy-description-1", 12.0), 35),
+                        Item(Product(2, "another-dummy-description-1", 15.0), 4),
+                    ],
+                ),
+                Order(
+                    shipping_address=Address(
+                        "dummy-house", "dummy-street",
+                        "dummy-city", "dummy-zip", Country.UKRAINE.value
+                    ),
+                    items=[
+                        Item(Product(3, "dummy-description-2", 10.0), 10),
+                        Item(Product(4, "another-dummy-description-3", 12.0), 7),
+                    ],
+                ),
+            ]),
+            1,
+            [
+                Order(
+                    shipping_address=Address(
+                        "dummy-house", "dummy-street",
+                        "dummy-city", "dummy-zip", Country.UNITED_KINGDOM.value
+                    ),
+                    items=[
+                        Item(Product(1, "dummy-description-1", 12.0), 35),
+                        Item(Product(2, "another-dummy-description-1", 15.0), 4),
+                    ],
+                ),
+            ],
+            id="List orders by product id"
+        ),
+    ]
+)
+def test_list_orders_by_product(history, product_id, expected_orders):
+    assert history.list_orders_by_product(product_id) == expected_orders
